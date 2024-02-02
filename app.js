65ecd3be530bf445bc10254d5e67cc678a7c7d6b
Rust-Hack <gleb.aleksandrovich.rust@gmail.com>
2024-02-03 02:13:03 +0300

    burger

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ function toggleMenu() {
     var links = document.querySelector('.links');
     var cross = document.querySelector('.cross');
 
-    if (links.style.display === 'flex') {
-        links.style.display = 'none';
-        cross.style.display = 'none';
-    } else {
-        links.style.display = 'flex';
-        cross.style.display = 'block';
-    }
+    links.classList.toggle('active');
+    cross.classList.toggle('active');
 }
